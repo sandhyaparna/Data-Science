1207216013da62cb6b18d073e58dad46a16fc70e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-16 17:09:26 -0500

    Update Example.py

--- TimeSeries/Example.py
+++ TimeSeries/Example.py
@@ -1,2 +1,27 @@
 # Real world example
 # https://medium.com/open-machine-learning-course/open-machine-learning-course-topic-9-time-series-analysis-in-python-a270cb05e0b3
+
+import numpy as np                               # vectors and matrices
+import pandas as pd                              # tables and data manipulations
+import matplotlib.pyplot as plt                  # plots
+import seaborn as sns                            # more plots
+
+from dateutil.relativedelta import relativedelta # working with dates with style
+from scipy.optimize import minimize              # for function minimization
+
+import statsmodels.formula.api as smf            # statistics and econometrics
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+
+from itertools import product                    # some useful functions
+from tqdm import tqdm_notebook
+
+import warnings                                  # `do not disturbe` mode
+
+
+
+
+
+
+
