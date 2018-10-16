c464b64e496285aa0fab65fb2679fec631360d24
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-16 16:51:58 -0500

    Update AnomalyAlgorithms.py

--- Anomaly Detection/AnomalyAlgorithms.py
+++ Anomaly Detection/AnomalyAlgorithms.py
@@ -1,1 +1,18 @@
+# luminol
+# https://github.com/linkedin/luminol
+
+# Moving Avergae Based
+# https://www.datascience.com/blog/python-anomaly-detection
+
+# https://github.com/rob-med/awesome-TS-anomaly-detection
+
+
+
+
+
+
+
+
+
+
 
