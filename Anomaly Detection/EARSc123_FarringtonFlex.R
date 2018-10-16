f26471b387a7bc007cb39ae61f25e8b502622506
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-16 16:57:55 -0500

    Create EARSc123_FarringtonFlex.R

---
+++ Anomaly Detection/EARSc123_FarringtonFlex.R
@@ -0,0 +1,10 @@
+# Surveillance algorithms in R
+https://cran.r-project.org/web/packages/surveillance/surveillance.pdf
+
+# Farrington
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.460.6204&rep=rep1&type=pdf
+
+# EARC 
+https://rdrr.io/rforge/surveillance/man/earsC.html
+
+
