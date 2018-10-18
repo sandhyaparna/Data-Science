ecf36ae62ce6b3f893d405309341331a30391088
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-18 10:59:04 -0500

    Update Example.py

--- TimeSeries/Example.py
+++ TimeSeries/Example.py
@@ -25,7 +25,41 @@ import warnings                                  # `do not disturbe` mode
 # Convert data frame to TS - Date column has to be converted to index 
 Df = Df.set_index('DateTime_Var')
 
+# Data can be aggregared to different levels - likely Dates can be converted to weekly date, monthly date etc
+# https://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.resample.html
+# Numeric Var is aggregated to level of date mentioned in replace function
+Df['Numeric_Var'].replace('MS').sum
 
+resample('M', convention='end') - # Month end date
+resample('M')
+
+B       business day frequency
+C       custom business day frequency (experimental)
+D       calendar day frequency
+W       weekly frequency
+M       month end frequency
+SM      semi-month end frequency (15th and end of month)
+BM      business month end frequency
+CBM     custom business month end frequency
+MS      month start frequency
+SMS     semi-month start frequency (1st and 15th)
+BMS     business month start frequency
+CBMS    custom business month start frequency
+Q       quarter end frequency
+BQ      business quarter endfrequency
+QS      quarter start frequency
+BQS     business quarter start frequency
+A       year end frequency
+BA      business year end frequency
+AS      year start frequency
+BAS     business year start frequency
+BH      business hour frequency
+H       hourly frequency
+T       minutely frequency
+S       secondly frequency
+L       milliseonds
+U       microseconds
+N       nanoseconds
 
 
 
